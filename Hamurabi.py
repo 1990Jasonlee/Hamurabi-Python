0065aa2e1f3be38bf1456f3083ed474f81c6d6b4
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-03-19 15:58:06 -0400

    update

--- Hamurabi.py
+++ Hamurabi.py
@@ -1,3 +1,5 @@
+from random import random
+
 def play_game():
 # Default variables
     year = 1
@@ -91,6 +93,51 @@ def play_game():
         return acres_planted
 
 
+    def plague(population):
+            if random.randint(0, 99) < 15:
+                return population
+            else:
+                return 0
+    def starved_deaths(population, bushels_to_feed_people):
+        people_starved = population - bushels_to_feed_people / 20
+        if people_starved > 0:
+            return people_starved
+        else:
+            return 0
+
+    def immigrants(population, acres_owned, grain_in_storage,):
+        if immigrants < 0:
+            immigrants= int((20 * acres_owned + grain_in_storage) / ((100 * population) + 1))
+        else:
+            return int(immigrants)
+
+
+    def uprising(population, people_starved):
+        percentage =  people_starved / population;
+        if percentage > 45:
+            return 0
+        else:
+            return uprising
+
+
+    def harvest(acres, bushels_used_as_seed):
+        bushels_used_as_seed = acres * harvest
+        return bushels_used_as_seed
+
+
+    def grain_eaten_by_rats(harvest):
+        if random.randint(0, 99) < 40:
+            bushels_destroyed = random.randint(10, 30) * harvest / 100
+            return bushels_destroyed
+        else:
+            return 0
+
+
+
+    def new_cost_of_land():
+            new_cost_of_land = random.randint(17, 23)
+            return new_cost_of_land
+
     game_over = False
     while year <= 10 and not game_over:
         summary()
@@ -100,7 +147,7 @@ def play_game():
             if choice == 1:
                 ask_how_many_acres_to_buy(new_cost_of_land, bushels)
                 bushels_spent = ask_how_many_acres_to_buy(new_cost_of_land, bushels)
-                # # acres = acres + (bushels - bushels_spent) / new_cost_of_land
+                # acres = acres + (bushels - bushels_spent) / new_cost_of_land
                 # bushels = bushels_spent
                 break
             elif choice == 2:
